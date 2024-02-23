64c86f0d923c3bc880f637144ac1d9b028ef6018
sethdanny <naddulidaniel1994@gmail.com>
2024-02-24 00:30:49 +0300

    created error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import morgan from 'morgan';
+import dbConnect from './config/database.js';
+import errorHandler from './middleware/errors.js';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -10,8 +13,22 @@ app.get('/', (req, res) => {
 })
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(morgan('dev'));
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
-});
+
+app.use(errorHandler);
+
+const startServer = async () => {
+    try {
+      await dbConnect();
+      app.listen(PORT, () => {
+        console.log(`Server is listening on: http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  startServer();

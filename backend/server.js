289c9adc3383848f818036f6db1eab69061851a4
nightburnn <nightburn2003@gmail.com>
2024-03-03 20:07:33 +0100

    open frontend from server

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,16 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from 'passport';
 import MongoStore from 'connect-mongo';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 import userRoutes from './routes/user.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(dirname(__filename)); // Go up one level
+
 const app = express();
 const PORT = process.env.PORT || 5001;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,35 +25,45 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(cookieParser());
 
-
 app.use(session({
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: true,
   cookie: {
-		maxAge: 24 * 60 * 60 * 1000,
-	},
+    maxAge: 24 * 60 * 60 * 1000,
+  },
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI,
-
   })
-}))
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Serve static files from the 'frontend' folder
+app.use(express.static(join(__dirname, 'frontend')));
+console.log(join(__dirname, 'frontend'));
+
+
+// Define your API routes
 app.use('/api/users', userRoutes);
 
+// Catch-all route to serve the 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'frontend', 'index.html'));
+});
+
+// Error handling middleware
 app.use(errorHandler);
 
 const startServer = async () => {
-    try {
-      await dbConnect();
-      app.listen(PORT, () => {
-        console.log(`Server is listening on: http://localhost:${PORT}`);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  startServer();
+  try {
+    await dbConnect();
+    app.listen(PORT, () => {
+      console.log(`Server is listening on: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();

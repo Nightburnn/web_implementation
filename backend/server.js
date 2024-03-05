1623c19d4beafdaddb48babd77cf2e6bd0890773
nightburnn <nightburn2003@gmail.com>
2024-03-05 21:18:03 +0100

    fix signup and login error message

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import morgan from 'morgan';
-import dbConnect from './config/database.js';
-import errorHandler from './middleware/errors.js';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import passport from 'passport';
-import MongoStore from 'connect-mongo';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+import dbConnect from "./config/database.js";
+import errorHandler from "./middleware/errors.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-import userRoutes from './routes/user.js';
+import userRoutes from "./routes/user.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(dirname(__filename)); // Go up one level
@@ -22,34 +22,35 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
-app.use(session({
-  secret: process.env.SECRET_KEY,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-  },
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
   })
-}));
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Serve static files from the 'frontend' folder
-app.use(express.static(join(__dirname, 'frontend')));
-console.log(join(__dirname, 'frontend'));
-
+app.use(express.static(join(__dirname, "frontend")));
+console.log(join(__dirname, "frontend"));
 
 // Define your API routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 // Catch-all route to serve the 'index.html'
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'frontend', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "frontend", "index.html"));
 });
 
 // Error handling middleware
@@ -61,6 +62,7 @@ const startServer = async () => {
     app.listen(PORT, () => {
       console.log(`Server is listening on: http://localhost:${PORT}`);
     });
+    
   } catch (error) {
     console.log(error);
   }
--- frontend/features/signup.js
+++ frontend/features/signup.js
@@ -1,46 +1,59 @@
-document.getElementById('signupForm').addEventListener('submit', async function (event) {
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
-  
-    const fullName = document.getElementById('fullName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
+
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
     try {
-      const response = await fetch('http://localhost:5000/api/users/register', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/api/users/register", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ fullName, email, password }),
       });
-  
+
       if (response.ok) {
-        showSuccessAlert('Signup successful!');
+        showSuccessAlert("Signup successful!");
       } else {
-        const errorData = await response.json(); // Assuming the server sends JSON error messages
-        showErrorAlert(errorData.error || 'Password is incorrect');
+        const errorData = await response.json();
+
+        // Check for the specific error message related to email already registered
+        if (
+          response.status === 409 &&
+          errorData.message === "Email already been registered"
+        ) {
+          showErrorAlert("Email is already registered");
+        } else {
+          showErrorAlert(
+            errorData.message || "Password must be at least 8 characters"
+          );
+        }
       }
     } catch (error) {
-      console.error('Network error:', error);
-      showErrorAlert('Network error');
+      console.error("Network error:", error);
+      showErrorAlert("Network error");
     }
   });
-  
-  function showSuccessAlert(message) {
-    Swal.fire({
-      icon: 'success',
-      title: 'Success',
-      text: message,
-    }).then(() => {
-      // Callback function to execute after the user clicks "OK"
-      window.location.href = './login.html'; // Redirect to the login page
-    });
-  }
-  
-  function showErrorAlert(errorMessage) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Error during signup',
-      text: errorMessage,
-    });
-  }
+
+function showSuccessAlert(message) {
+  Swal.fire({
+    icon: "success",
+    title: "Success",
+    text: message,
+  }).then(() => {
+    // Callback function to execute after the user clicks "OK"
+    window.location.href = "./login.html"; // Redirect to the login page
+  });
+}
+
+function showErrorAlert(errorMessage) {
+  Swal.fire({
+    icon: "error",
+    title: "Error during signup",
+    text: errorMessage,
+  });
+}

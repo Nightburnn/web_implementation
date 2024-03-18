90da074ac7269684e6940b772079151e1045c188
Nightburn <nightburn2003@gmail.com>
2024-03-18 19:00:35 +0100

    Update login.js

--- frontend/features/login.js
+++ frontend/features/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('https://traq-986l.onrender.com/api/users/login', {
+    const response = await fetch('http://localhost:5000/api/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

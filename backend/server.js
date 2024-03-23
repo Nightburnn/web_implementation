b0e84a875822d2e9e463e82d2a775107b0b1307a
nightburnn <nightburn2003@gmail.com>
2024-03-23 23:53:41 +0100

    sync account

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,6 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 import userRoutes from './routes/user.js';
-import newsController from '../controllers/newsController.js';
-import transactionController from '../controllers/transactionController.js';
-import accountController from '../controllers/accountController.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename); // Go up one level
@@ -49,19 +46,7 @@ app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('Hello from the server side!');
 });
-// Transaction Routes
-app.post('/api/transactions', transactionController.createTransaction);
-app.get('/api/transactions', transactionController.getAllTransactions);
-app.get('/api/transactions/:id', transactionController.getTransactionById);
-app.put('/api/transactions/:id', transactionController.updateTransaction);
-app.delete('/api/transactions/:id', transactionController.deleteTransaction);
 
-// Account Routes
-app.post('/api/accounts', accountController.createAccount);
-app.get('/api/accounts', accountController.getAllAccounts);
-app.get('/api/accounts/:id', accountController.getAccountById);
-app.put('/api/accounts/:id', accountController.updateAccount);
-app.delete('/api/accounts/:id', accountController.deleteAccount);
 // Error handling middleware
 app.use(errorHandler);
 
